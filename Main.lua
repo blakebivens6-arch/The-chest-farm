cb88fdd89f63d8be494c01e6df35672ab8fae96f
blakebivens6-arch <blakebivens6@gmail.com>
2025-11-21 00:56:26 -0500

    Remove duplicate ChestFarm.lua loading
    Removed duplicate loading of ChestFarm.lua.

--- Main.lua
+++ Main.lua
@@ -2,5 +2,5 @@
 -- Entry point, loads Loader and ChestFarm
 
 -- Replace RAW URLs with your GitHub raw links
-loadstring(game:HttpGet("https://raw.githubusercontent.com/blakebivens6-arch/The-chest-farm/refs/heads/main/ChestFarm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/blakebivens6-arch/The-chest-farm/refs/heads/main/Loader.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/blakebivens6-arch/The-chest-farm/refs/heads/main/ChestFarm.lua"))()
