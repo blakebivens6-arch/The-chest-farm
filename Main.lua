cdbe15526043950cacc387da86680428746930ad
blakebivens6-arch <blakebivens6@gmail.com>
2025-11-21 00:54:04 -0500

    Create Main.lua to load external scripts
    Added entry point to load Loader and ChestFarm scripts.

---
+++ Main.lua
@@ -0,0 +1,6 @@
+-- Main.lua
+-- Entry point, loads Loader and ChestFarm
+
+-- Replace RAW URLs with your GitHub raw links
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_GITHUB_REPO/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_GITHUB_REPO/ChestFarm.lua"))()
